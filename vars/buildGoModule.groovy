94726fd1efb10d2d7b50fa28130a5545d6235a33
Nurshinov <alimzhan.nurshinov@gmail.com>
2022-09-12 00:15:48 +0300

    created some pipes

--- vars/buildGoModule.groovy
+++ vars/buildGoModule.groovy
@@ -1,19 +1,43 @@
 def call() {
     node {
-    timestamps {
-        stage('Checkout') {
-            println "Выкачиваем репозиторий с исходным кодом"
-            git_checkout = checkout scm
-            println git_checkout
+        ansiColor('xterm') {
+            timestamps {
+                try {
+                    def playBook = libraryResource("deploy_app.yml")
+                    def hosts = libraryResource("hosts")
+                    def ansibleCfg = libraryResource("ansible.cfg")
+                    stage('Checkout') {
+                        println "Выкачиваем репозиторий с исходным кодом"
+                        git_checkout = checkout scm
+                        println git_checkout
+                    }
+                    stage('Build') {
+                        println "Собираем"
+                        sh "go build -o online_shop"
+                        binPath = sh(script: "pwd", returnStdout: true).trim().toString() + "/online_shop"
+                    }
+                    stage('Deploy') {
+                        withEnv(["ANSIBLE_CONFIG=ansible.cfg"]) {
+                            println "Заливаем через ансибл"
+                            writeFile file: 'playbook.yml', text: playBook
+                            writeFile file: 'hosts', text: hosts
+                            writeFile file: 'ansible.cfg', text: ansibleCfg
+                            ansiblePlaybook(
+                                    credentialsId: 'private_key',
+                                    inventory: "hosts",
+                                    playbook: "playbook.yml",
+                                    colorized: true
+//                            vaultCredentialsId: 'ansible_vault_credentials'
+                            )
+                        }
+                    }
+                    cleanWs()
+                } catch (exception) {
+                    println "Сборка упала: " + "\n${exception}"
+                    currentBuild.result = 'FAILURE'
+                    cleanWs()
+                }
+            }
         }
-        stage('Build') {
-            println "Собираем"
-            sh "go build -o online_shop"
-        }
-        stage('Deploy') {
-            println "Заливаем через ансибл"
-        }
-        cleanWs()
-    }
     }
 }
