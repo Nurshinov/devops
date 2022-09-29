1f2ba4735ec4198e5cbd2414a188d42fbef165d5
Nurshinov <alimzhan.nurshinov@gmail.com>
2022-09-29 14:13:11 +0300

    Добавил пайп для  js модуля

---
+++ vars/buildJSModule.groovy
@@ -0,0 +1,26 @@
+APP_VERSION = ${BUILD_NUMBER}
+def call(String APP_NAME) {
+    node {
+        ansiColor('xterm') {
+            timestamps {
+                try {
+                    stage('Checkout') {
+                        println "Выкачиваем репозиторий с исходным кодом"
+                        git_checkout = checkout scm
+                        println git_checkout
+                    }
+                    stage('Собираем docker образ') {
+                        sh "docker build -t  ${APP_NAME}:${APP_VERSION} ."
+                    }
+                    stage('Пушим образ и запускаем') {
+                        sh "docker tag ${APP_NAME}:${APP_VERSION} docker.registry.ext:3000/${APP_NAME}:${APP_VERSION}"
+                    }
+                } catch(exception){
+                    println "Сборка упала: " + "\n${exception}"
+                    currentBuild.result = 'FAILURE'
+                    cleanWs()
+                }
+            }
+        }
+    }
+}
