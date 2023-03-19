a0cd3a54e12f96ecd9c63af099a9453ac425a98c
Nurshinov <alimzhan.nurshinov@gmail.com>
2023-03-19 22:42:04 +0300

    fixed toLowerCase

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -10,7 +10,7 @@ def call(String appName) {
                     }
                     stage('Build') {
                         println "Собираем образ и пушим в registry"
-                        sh "docker build -t ${appName} ."
+                        sh "docker build -t ${appName.toLowerCase()} ."
                         branch = git_checkout.branch == "master" ? "latest" : git_checkout.branch
                         sh "docker push ${DOCKER_REGISTRY_REPO}/${appName.toLowerCase()}:${branch}"
                     }
